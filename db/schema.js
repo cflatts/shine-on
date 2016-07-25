96ad50288d1b6a7af6c4a6c0100c8eb9f8905bc6
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-25 13:08:46 -0500

    created question schema for server side

--- db/schema.js
+++ db/schema.js
@@ -10,13 +10,25 @@ const usersSchema = new Schema({
   email:     { type: String, required: true },
   password:  { type: String, required: true },
   // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-  
+
    // example of optional fields
   name:      { type: String },
   createdAt: { type: Date, default: Date.now }
 
 })
 
+const questionSchema = new Schema ({
+    title: {type: String, required: true},
+    content: {type: String},
+    answered: {type: Boolean, default: false},
+    tags: {type: [String], required: true},
+    // {timestamp: true} how does this work?
+    authorId: {type: String, required: true},
+    username: {type: String} //do I put this here or on the user schema?
+})
+
 module.exports = {
   User: createModel('User', usersSchema)
 }
+
+//what do I need to do to allow people replying to display their credentials?
