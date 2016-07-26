a2ef6dc948e581f57a2299f17f0d370c7575f3c1
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-26 14:45:26 -0500

    removed hardcoding to test everything worked

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,11 +5,11 @@ import init from './init'
 
 
 const app = function() {
-  document.querySelector('.container').innerHTML = "<h1>Woah!</h1>"
+
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
-// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
+// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE.
 export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
