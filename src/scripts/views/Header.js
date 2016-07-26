3ec7a457284ae88126dccd43eaa88c4f8091bb8e
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-26 18:34:47 -0500

    created wrapping div around header html

--- src/scripts/views/Header.js
+++ src/scripts/views/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Header = React.createClass({
     render: function() {
         return (
-            <h1>Shine On!</h1>
+            <div className = 'header'>
+                <h1>Shine On!</h1>
+            </div>
             )
     }
 })
