44f77bf21707eeecaab1573a025ff06114b7ea6a
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-27 12:10:29 -0500

    created navigation bar

--- src/scripts/views/Header.js
+++ src/scripts/views/Header.js
@@ -5,8 +5,21 @@ const Header = React.createClass({
         return (
             <div className = 'header'>
                 <h1>Shine On!</h1>
+                <Navigation />
             </div>
             )
+    }
+})
+
+const Navigation = React.createClass ({
+    render: function() {
+        return (
+            <div className = 'navigation'>
+                <a href = '#home'>Home</a>
+                <a href = '#dashboard'>Dash</a>
+                <a href = '#myQuestions'>My Questions</a>
+            </div>
+        )
     }
 })
 
