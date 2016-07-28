51efefb107c59e6051f9165ef859e1e6d5a1f6e7
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-28 10:43:34 -0500

    rendered single view with a header

---
+++ src/scripts/views/singleQuestionView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const SingleQuestionView = React.createClass ({
+    render: function() {
+        return (
+            <div className = 'singleQuestion'>
+                <Header />
+            </div>
+        )
+    }
+})
+
+export default SingleQuestionView
