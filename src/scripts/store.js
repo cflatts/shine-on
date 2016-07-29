3f90bb7903a542a3568b92a2145bb090f1098baf
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-29 11:45:36 -0500

    initialized backbone events

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -6,7 +6,13 @@ const STORE = _.extend( Backbone.Events, {
 
     data: {
         collection: new QuestionCollection()
+    },
+
+    _initialize: function() {
+        this.data.collection.on('sync unpdate', this._broadcastChange.bind(this))
     }
 })
+
+STORE._initialize()
 
 export default STORE
