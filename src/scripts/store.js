e8e73119df13b225c254e355ce1394cda6ca779a
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-29 11:15:52 -0500

    created backbone events object and set it up to be extendable with underscore

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,3 +1,10 @@
 import _ from 'underscore'
 import Backbone from 'backbone'
 import {QuestionCollection} from './models/models'
+
+const STORE = _.extend( Backbone.Events, {
+
+
+})
+
+export default STORE
