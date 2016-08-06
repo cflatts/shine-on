711d653fdfe58e01510ce85af97ec4afebc0e019
Caitlin Flattery <caitlinflattery@gmail.com>
2016-08-06 09:33:56 -0500

    created filter handler

--- src/scripts/views/Filter.js
+++ src/scripts/views/Filter.js
@@ -2,13 +2,18 @@ import React from 'react'
 import DashboardView from './dashboardView'
 
 const FilterBar = React.createClass ({
+
+    _handleFilter: function(evt) {
+        ACTIONS.filterQuestions(evt.target.value)
+    },
+
     render: function() {
         return(
             <div className = 'filterBar'>
-                <button>Science</button>
-                <button>Technology</button>
-                <button>Engineering</button>
-                <button>Math</button>
+                <button value  = 'Science'onClick = {this._handleFilter}>Science</button>
+                <button value = 'Technology' onClick = {this._handleFilter}>Technology</button>
+                <button value = 'Engineering' onClick = {this._handleFilter}>Engineering</button>
+                <button value = 'Math' onClick = {this._handleFilter}>Math</button>
             </div>
         )
     }
