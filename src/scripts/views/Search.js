065fe6c83b739c8b55b177e8c537665b4a05b466
Caitlin Flattery <caitlinflattery@gmail.com>
2016-08-09 14:14:01 -0500

    bais structure of search module

--- src/scripts/views/Search.js
+++ src/scripts/views/Search.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 const SearchBar = React.createClass ({
 
+    render: function() {
+        return (
+            <div className = 'search'>
+                <input></input>
+            </div>)
+    }
 })
 
 export default SearchBar
