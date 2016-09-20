0d11d57092209c334ec3c74712223b633382e797
Caitlin Flattery <caitlinflattery@gmail.com>
2016-09-20 16:04:02 -0500

    exported message view

--- src/scripts/views/messageView.js
+++ src/scripts/views/messageView.js
@@ -4,3 +4,9 @@ import STORE from '../store'
 import ACTIONS from '../actions'
 import {User, QuestionModel, QuestionCollection, AnswerModel, AnswerCollection, MessageModel, MessageCollection} from '../models/models'
 import Footer from './Footer'
+
+const MessageView = React.createClass ({
+
+})
+
+export default MessageView
