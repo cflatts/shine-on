dc064a6e24fec7ad419c0ab02d51405591a39c0a
Caitlin Flattery <caitlinflattery@gmail.com>
2016-09-20 15:53:59 -0500

    created message view

---
+++ src/scripts/views/messageView.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Header from './Header'
+import STORE from '../store'
+import ACTIONS from '../actions'
+import {User, QuestionModel, QuestionCollection, AnswerModel, AnswerCollection, MessageModel, MessageCollection} from '../models/models'
+import Footer from './Footer'
